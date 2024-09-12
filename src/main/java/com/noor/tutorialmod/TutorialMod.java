8c66e6b8e36132d239938750c7cf5ab2a3b434d9
NoorAissat <nooraissat@gmail.com>
2024-09-12 03:03:00 -0400

    item add

--- src/main/java/com/noor/tutorialmod/TutorialMod.java
+++ src/main/java/com/noor/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package com.noor.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import com.noor.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -30,6 +33,7 @@ public class TutorialMod{
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+        ModItems.register(modEventBus);
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
@@ -42,6 +46,9 @@ public class TutorialMod{
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event){
+        if (event.getTabKey()== CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.ALEXANDRITE);
+        }
 
     }
 
